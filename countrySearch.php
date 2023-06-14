091e732392787d6fbd6c0d4959831da1a9b679ad
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2023-06-14 12:54:30 -0700

    Create countrySearch.php

---
+++ countrySearch.php
@@ -0,0 +1,30 @@
+<?php
+include("libs/functions.php");
+include("includes/header.php");
+?>
+
+
+
+<h2>Country</h2>
+
+<div class="row">
+	
+
+  	<img src="imgs/world.jpg" class=" d-block img" alt="...">
+
+  <div class="col-md-3 country">
+    <input class="magicInput" data-tbl="countries" placeholder="Search Country">
+    <div class=" magicList"></div>
+  </div>
+
+<!--   <div class="col-md-6">
+    <input class="magicInput" data-tbl="provinces" placeholder="Search..">
+    <div class=" magicList"></div>
+  </div> -->
+
+</div>
+
+
+</body>
+
+</html>
